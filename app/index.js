369e89c6c93ca9dd83c5555bd55438b11d0e52bc
yosef lakew <80267669+yoseflakew25@users.noreply.github.com>
2023-10-30 11:48:49 +0300

    .

--- app/index.js
+++ app/index.js
@@ -1,50 +1,5 @@
-import { StyleSheet, Text, View,ScrollView,SafeAreaView } from 'react-native'
-import React from 'react'
-import { Stack, useRouter } from 'expo-router'
-import { useState } from 'react'
-import { icons, COLORS,  SIZES,images } from '../constants'
+import { Redirect } from "expo-router";
 
-import {
-  ScreenHeaderBtn,
-  Welcome,
-  Nearbyjobs,
-  Popularjobs,
-} from "../components";
-
-
-const Home = () => {
-    const router = useRouter();
-
-  return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
-          <Stack.Screen
-              options={{
-                  headerStyle: { backgroundColor: COLORS.lightWhite },
-                  headerShadowVisible: false,
-                  headerLeft: () => (
-                      <ScreenHeaderBtn iconUrl={ icons.menu} dimension="60%" />
-                  ),
-                  headerRight: () => (
-                      <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
-                  ),
-                  headerTitle: ""
-              }} />
-          <ScrollView showsVerticalScrollIndicator={false}>
-              <View
-                  style={{
-                      flex: 1,
-                      padding: SIZES.medium
-                  }}
-              >
-                  <Welcome />
-                  <Popularjobs />
-                  <Nearbyjobs />
-              </View>
-          </ScrollView>
-    </SafeAreaView>
-  );
+export default function Index() {
+  return <Redirect href="/home" />;
 }
-
-export default Home;
-
-const styles = StyleSheet.create({})
